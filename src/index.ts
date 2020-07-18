ca214a821cce3509d26be4af4f60ecacf5a1799a
Rus Hughes <russell.hughes@gmail.com>
2020-07-18 12:20:52 +0200

    overriding google map with custom map to make it less complicated

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 import { User } from './User';
 
+const customMap = new CustomMap();
 const company = new Company();
 const user = new User();
 
 console.log(company);
 console.log(user);
-console.log(google);
+console.log(customMap);
