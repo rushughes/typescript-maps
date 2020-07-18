652dbe737843c454d915494aff4d598d522662d8
Rus Hughes <russell.hughes@gmail.com>
2020-07-18 12:22:00 +0200

    customMap now has the div id passed in to the constructor, rather than hard coded

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Company } from './Company';
 import { CustomMap } from './CustomMap';
 import { User } from './User';
 
-const customMap = new CustomMap();
+const customMap = new CustomMap('map');
 const company = new Company();
 const user = new User();
 
